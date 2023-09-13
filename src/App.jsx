6a432a68700eba0290dd0596740829dc5e33c5cf
goncharovastacy <gggnchr@gmail.com>
2023-09-13 20:12:10 +0300

    added validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
-import Comment from "./components/Comment";
-import { useState } from "react";
+import Comments from "./components/Comments";
 import "./App.scss";
 
 function App() {
-  let [commentText, setCommentText] = useState("");
-  const [commentsArray, setCommentsArray] = useState([]);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newArr = [checkSpam(commentText), ...commentsArray];
-    setCommentsArray(newArr);
-    setCommentText("");
-  };
-  const checkSpam = (comment) => {
-    return comment.replace(/viagra/gi, "***").replace(/xxx/gi, "***");
-  };
-  const handleChange = (e) => {
-    setCommentText((commentText = e.target.value));
-  };
-
-  return (
-    <div className="container">
-      <form>
-        <input type="text" onChange={handleChange} value={commentText} />
-        <button onClick={handleSubmit}>Send</button>
-      </form>
-      <div className="comments">
-        {commentsArray.map((el, i) => (
-          <Comment
-            text={el}
-            key={i}
-            className={i === 0 ? "new-comment comment" : "comment"}
-          ></Comment>
-        ))}
-      </div>
-    </div>
-  );
+  return <Comments></Comments>;
 }
 
 export default App;
---
+++ src/components/Comments.jsx
@@ -0,0 +1,46 @@
+import Comment from "./Comment";
+import { useState } from "react";
+
+function Comments() {
+  let [commentText, setCommentText] = useState("");
+  const [commentsArray, setCommentsArray] = useState([]);
+  const [error, setError] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (commentText !== "") {
+      const newArr = [checkSpam(commentText), ...commentsArray];
+      setCommentsArray(newArr);
+      setCommentText("");
+    } else {
+      setError("Пожалуйста, заполните поле");
+    }
+  };
+  const checkSpam = (comment) => {
+    return comment.replace(/viagra/gi, "***").replace(/xxx/gi, "***");
+  };
+  const handleChange = (e) => {
+    setCommentText((commentText = e.target.value));
+    setError("");
+  };
+
+  return (
+    <div className="container">
+      <span>{error}</span>
+      <form>
+        <input type="text" onChange={handleChange} value={commentText} />
+        <button onClick={handleSubmit}>Оставить комментарий</button>
+      </form>
+      <div className="comments">
+        {commentsArray.map((el, i) => (
+          <Comment
+            text={el}
+            key={i}
+            className={i === 0 ? "new-comment comment" : "comment"}
+          ></Comment>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Comments;
